527949a51efcbdfdf8eebd643ce293869c3172d3
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-03 18:56:59 -0400

    added previous assignment file for carryover

---
+++ Assignments/PDP-Assignment-7/src/freecell/bean/Card.java
@@ -0,0 +1,97 @@
+package freecell.bean;
+
+import java.util.Objects;
+
+import util.Utils;
+
+/**
+ * This class represents a Card. A {@link Card} has a {@link Suit} and {@link CardValue} associated
+ * with it.
+ */
+public class Card {
+
+  private final Suit suit;
+  private final CardValue cardValue;
+
+  /**
+   * Constructs a Card object with the given suit and cardValue.
+   *
+   * @param suit      the suit of the card
+   * @param cardValue the value for the card
+   * @throws IllegalArgumentException if the given params are null
+   */
+  public Card(Suit suit, CardValue cardValue) throws IllegalArgumentException {
+    this.suit = Utils.requireNonNull(suit);
+    this.cardValue = Utils.requireNonNull(cardValue);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (!(o instanceof Card)) {
+      return false;
+    }
+
+    Card card = (Card) o;
+    return this.getSuit() == card.getSuit()
+            && this.getCardValue() == card.getCardValue();
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.getSuit(), this.getCardValue());
+  }
+
+  /**
+   * Returns the string representation of the card. The string representation will be in following
+   * format: "[value of the card][suit symbol]". If a card is Ace of Diamonds, then its string
+   * representation will be "A♦". If a card is Ten of Diamonds, then its string representation will
+   * be "10♦".
+   *
+   * @return the string representation of the card
+   */
+  @Override
+  public String toString() {
+    return this.getCardValue().getSymbol() + this.getSuit().getSymbol();
+  }
+
+  /**
+   * Returns the suit of this card.
+   *
+   * @return the suit of this card
+   */
+  public Suit getSuit() {
+    return this.suit;
+  }
+
+  /**
+   * Returns the value of this card.
+   *
+   * @return the value of this card
+   */
+  public CardValue getCardValue() {
+    return this.cardValue;
+  }
+
+  /**
+   * Returns a card by parsing the given string. It throws an {@link IllegalArgumentException} if
+   * the given param is null or given string is non-parsable.
+   *
+   * @param cardString the string to parse
+   * @return a card by parsing the given string
+   * @throws IllegalArgumentException if the given param is null or given string is non-parsable.
+   */
+  public static Card parse(String cardString) throws IllegalArgumentException {
+    Utils.requireNonNull(cardString);
+    cardString = cardString.trim();
+    if (cardString.length() > 3 || cardString.length() < 2) {
+      throw new IllegalArgumentException("Invalid string");
+    }
+    char suitSymbol = cardString.charAt(cardString.length() - 1);
+    String cardValueString = Utils.removeTheLastCharacterFrom(cardString);
+    return new Card(Suit.parse(suitSymbol), CardValue.parse(cardValueString));
+  }
+}
---
+++ Assignments/PDP-Assignment-7/src/freecell/bean/PileCategory.java
@@ -0,0 +1,139 @@
+package freecell.bean;
+
+import java.util.List;
+
+import freecell.model.PileType;
+import util.Utils;
+
+/**
+ * This enum represents Category for the three types of piles in a game of Freecell. <br> Open: This
+ * pile can hold only one card. It is like a buffer to temporarily hold cards <br> Cascade: This is
+ * a pile of face-up cards. A build within a cascade pile is a subset of cards that has
+ * monotonically decreasing values and suits of alternating colors<br> Foundation: Initially empty,
+ * there are 4 foundation piles, one for each suit. Each foundation pile collects cards in
+ * increasing order of value for one suit (Ace being the lowest). <br> The goal of the game is to
+ * fill up all the foundation piles
+ */
+public enum PileCategory {
+
+  OPEN('O', PileType.OPEN) {
+    /**
+     * Returns true if the given list is empty, false otherwise.
+     * @param card the card to check
+     * @param pile the list to check against given card
+     * @return true if the given pile is empty, false otherwise
+     */
+    @Override
+    public boolean canPutCardInPile(Card card, List<Card> pile) {
+      return pile.isEmpty();
+    }
+  },
+
+  CASCADE('C', PileType.CASCADE) {
+    /**
+     * Returns true if the given card can be placed at the end of the given list, false otherwise.
+     * The given card can only be placed at the end of the given list if and only if its color is
+     * different from that of the last card in the given list, and its value is exactly one less
+     * than that of the last card in the given list. If the given list is empty it will return true.
+     *
+     * @param card the card to check
+     * @param pile the list to check against given card
+     * @return true if the given card can be placed at the end of the given list, false otherwise
+     */
+    @Override
+    public boolean canPutCardInPile(Card card, List<Card> pile) {
+      if (pile.isEmpty()) {
+        return true;
+      } else {
+        Card lastCardInPile = Utils.getLast(pile);
+        return (lastCardInPile.getSuit().getColor() != card.getSuit().getColor())
+                && (lastCardInPile.getCardValue().getPriority() - card.getCardValue().getPriority()
+                == 1);
+      }
+    }
+  },
+
+
+  FOUNDATION('F', PileType.FOUNDATION) {
+    /**
+     * Returns true if the given card can be placed at the end of the given list, false otherwise.
+     * The given card can only be placed at the end of the given list if and only if its suit
+     * matches to that of the last card in the given list, and its value is one more than that of
+     * the last card of the given list. If the given list is empty it returns true.
+     *
+     * @param card the card to check
+     * @param pile the list to check against given card
+     * @return true if the given card can be placed at the end of the given list, false otherwise
+     */
+    @Override
+    public boolean canPutCardInPile(Card card, List<Card> pile) {
+      if (pile.isEmpty()) {
+        return card.getCardValue().equals(CardValue.ACE);
+      } else {
+        Card lastCardInPile = Utils.getLast(pile);
+        return (lastCardInPile.getSuit() == card.getSuit())
+                && (card.getCardValue().getPriority() - lastCardInPile.getCardValue().getPriority()
+                == 1);
+      }
+    }
+  };
+
+  private final char symbol;
+  private final PileType pileType;
+
+  /**
+   * Constructs a PileCategory Object with the given symbol and pileType.
+   *
+   * @param symbol   the given symbol
+   * @param pileType the given pileType
+   */
+  PileCategory(char symbol, PileType pileType) {
+    this.symbol = symbol;
+    this.pileType = pileType;
+  }
+
+  /**
+   * Returns true, if the given card can be added to the end of the given list, false otherwise.
+   *
+   * @param card the card to check
+   * @param pile the list to check against given card
+   * @return true, if the given card can be added to the end of the given list, false otherwise
+   */
+  public abstract boolean canPutCardInPile(Card card, List<Card> pile);
+
+  /**
+   * Returns the symbol of this PileCategory.
+   *
+   * @return the symbol of this PileCategory
+   */
+  public char getSymbol() {
+    return symbol;
+  }
+
+  /**
+   * Returns the pileType of this PileCategory.
+   *
+   * @return the pileType of this PileCategory
+   */
+  public PileType getPileType() {
+    return pileType;
+  }
+
+  /**
+   * Returns a PileCategory for the given PileType. It throws an {@link IllegalArgumentException} if
+   * there is no PileCategory associated with the given PileType.
+   *
+   * @param pileType the given pileType
+   * @return a PileCategory for the given PileType
+   * @throws IllegalArgumentException if there is not PileCategory associated with the given
+   *                                  PileType
+   */
+  public static PileCategory getPileCategory(PileType pileType) throws IllegalArgumentException {
+    for (PileCategory pileCategory : PileCategory.values()) {
+      if (pileCategory.getPileType().equals(pileType)) {
+        return pileCategory;
+      }
+    }
+    throw new IllegalArgumentException("Invalid input");
+  }
+}
