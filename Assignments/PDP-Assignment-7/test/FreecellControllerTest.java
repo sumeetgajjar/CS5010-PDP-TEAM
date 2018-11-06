4d9514b29c3968e6254a60554c995dfc944ba470
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-06 16:33:51 -0500

    1. added test to pass invalid params to the controller

--- Assignments/PDP-Assignment-7/test/FreecellControllerTest.java
+++ Assignments/PDP-Assignment-7/test/FreecellControllerTest.java
@@ -2,8 +2,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.StringReader;
+import java.util.List;
 
+import freecell.bean.Card;
 import freecell.controller.FreecellController;
+import freecell.model.FreecellMultiMoveModel;
+import freecell.model.FreecellOperations;
 
 /**
  * Represents tests that are run on the <code>FreecellController</code> that implements
@@ -36,5 +40,52 @@ public class FreecellControllerTest {
     } catch (IllegalArgumentException e) {
       Assert.assertEquals("Invalid input", e.getMessage());
     }
+  }
+
+  @Test
+  public void invalidModelOrDeckGivenToControllerFails() {
+    StringReader stringReader = new StringReader("C1 8 F1 q");
+    StringBuffer stringBuffer = new StringBuffer();
+    FreecellController freecellController = new FreecellController(stringReader, stringBuffer);
+    FreecellOperations<Card> freecellOperations = this.getFreecellOperation();
+
+    try {
+      freecellController.playGame(null, freecellOperations, false);
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("Invalid input", e.getMessage());
+    }
+
+    try {
+      freecellController.playGame(freecellOperations.getDeck(), null, false);
+      Assert.fail("should have failed");
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals("Invalid input", e.getMessage());
+    }
+  }
+
+  @Test
+  public void passingDeckWith51CardsToController() {
+    StringReader actualInput = new StringReader("C1 q");
+    StringBuffer actualOutput = new StringBuffer();
+    StringBuffer expectedOutput = new StringBuffer();
+
+    FreecellController freecellController = new FreecellController(actualInput, actualOutput);
+    FreecellOperations<Card> freecellOperations = this.getFreecellOperation();
+
+    List<Card> deck = freecellOperations.getDeck();
+    deck.remove(0);
+
+    freecellController.playGame(deck, freecellOperations, false);
+    expectedOutput.append(System.lineSeparator());
+    expectedOutput.append("Invalid move. Try again.");
+    expectedOutput.append(System.lineSeparator());
+    expectedOutput.append("Game quit prematurely.");
+    Assert.assertEquals(expectedOutput, actualOutput);
+    expectedOutput.append(System.lineSeparator());
+  }
+
+  private FreecellOperations<Card> getFreecellOperation() {
+    return FreecellMultiMoveModel.getBuilder().cascades(4).opens(4).build();
   }
 }
