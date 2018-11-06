edaeb7c734ed802ff34f1e44cd826b9b4c354f42
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-06 00:13:22 -0500

    added documentation in Test

---
+++ Assignments/PDP-Assignment-7/test/FreecellMultiMoveModelTest.java
@@ -0,0 +1,6 @@
+/**
+ * Represents tests that are run on the <code>FreecellMultiMoveModel</code> that implements
+ * <code>FreeCellOperations</code>.
+ */
+public class FreecellMultiMoveModelTest {
+}
