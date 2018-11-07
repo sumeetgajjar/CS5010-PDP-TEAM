88ac45d7419acb1479c1cc3f9428091904ee04f2
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-07 17:19:41 -0500

    1. transmitting informative message to user in case move fails

--- Assignments/PDP-Assignment-7/src/freecell/controller/FreecellController.java
+++ Assignments/PDP-Assignment-7/src/freecell/controller/FreecellController.java
@@ -159,8 +159,14 @@ public class FreecellController implements IFreecellController<Card> {
       this.transmitGameState(model);
     } catch (IllegalArgumentException e) {
       this.transmitMessage(String.format("%s: %s", INVALID_MOVE_MESSAGE_STRING,
-              e.getMessage()));
+              this.getInvalidMoveMessage(sourcePileInfo, cardIndex, destinationPileInfo)));
     }
+  }
+
+  private String getInvalidMoveMessage(PileInfo sourcePileInfo, int cardIndex,
+                                       PileInfo destinationPileInfo) {
+    return String.format("cannot move card of Index:%d from pile:%s to pile:%s",
+            (cardIndex + 1), sourcePileInfo, destinationPileInfo);
   }
 
   private boolean toQuit(String inputString) {
@@ -217,6 +223,11 @@ public class FreecellController implements IFreecellController<Card> {
 
     int getPileIndex() {
       return pileIndex;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("%s%d", pileCategory.getSymbol(), pileIndex + 1);
     }
   }
 }
