eb9e0fb42545f56c02f5108220a36b84519ac04c
Chintan Shah <chnsh24@gmail.com>
2018-11-07 16:48:51 -0500

    Documented rule checker

--- Assignments/PDP-Assignment-7/src/freecell/model/rulechecker/OpenPileRuleChecker.java
+++ Assignments/PDP-Assignment-7/src/freecell/model/rulechecker/OpenPileRuleChecker.java
@@ -5,11 +5,22 @@ import java.util.List;
 import freecell.bean.Card;
 
 /**
- * Created by gajjar.s, on 12:37 PM, 11/6/18
+ * OpenPileRuleChecker represents rules that apply to a move when the Open pile is the source or the
+ * destination of the move.
  */
-public class OpenPileRuleChecker extends AbstractRuleChecker<Card> {
+public class OpenPileRuleChecker implements RuleChecker<Card> {
+
+  /**
+   * Validates if a card can be put into the destinationPile passed into it.
+   *
+   * <p>A move to an open pile is valid if it's empty.
+   *
+   * @param card            the card that needs to be moved
+   * @param destinationPile the destination pile that it needs to be moved into
+   * @return true if the card can be put into the destination pile, false otherwise
+   */
   @Override
-  public boolean canPutCardsInPile(Card card, List<Card> pile) {
-    return pile.isEmpty();
+  public boolean canPutCardsInPile(Card card, List<Card> destinationPile) {
+    return destinationPile.isEmpty();
   }
 }
--- Assignments/PDP-Assignment-7/src/freecell/model/rulechecker/RuleChecker.java
+++ Assignments/PDP-Assignment-7/src/freecell/model/rulechecker/RuleChecker.java
@@ -3,10 +3,37 @@ package freecell.model.rulechecker;
 import java.util.List;
 
 /**
- * Created by gajjar.s, on 12:32 PM, 11/6/18
+ * RuleChecker represents a set of operations that helps validate a "move" in the game of free
+ * cell.
+ *
+ * <p>These operations are supposed to be used by free cell models in order to validate whether a
+ * card can be put into a destination pile and whether a card can be taken from a source pile.
  */
 public interface RuleChecker<K> {
-  boolean canPutCardsInPile(K card, List<K> pile);
+  /**
+   * Validates if a card can be put into the destinationPile passed into it. The concrete rules are
+   * model dependent.
+   *
+   * @param card            the card that needs to be moved
+   * @param destinationPile the destination pile that it needs to be moved into
+   * @return true if the card can be put into the destination pile, false otherwise
+   */
+  boolean canPutCardsInPile(K card, List<K> destinationPile);
 
-  boolean canGetCardsFromThePile(int cardIndex, List<K> pile);
+  /**
+   * Validates if a card can be taken out from the sourcePile. The concrete rules are model
+   * dependent.
+   *
+   * <p>The most common operation is that one can get just the last card from any pile that is
+   * the source of a move. A default implementation here helps make the code cleaner and avoid an
+   * abstract class.
+   *
+   * @param cardIndex  the card index of the source sourcePile
+   * @param sourcePile the source pile from which the card needs to be taken out
+   * @return true if the card can be taken out of the source pile, false otherwise
+   */
+  default boolean canGetCardsFromThePile(int cardIndex, List<K> sourcePile) {
+    // can only get last card and no other card
+    return sourcePile.size() != 0 && sourcePile.size() - 1 == cardIndex;
+  }
 }
