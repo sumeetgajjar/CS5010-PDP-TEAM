eef0c7ca3962fc143a7afa756cef269cdd9f1a1a
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-12 23:36:39 -0500

    1. added test to check for initialization and getting portfolios

--- Assignments/PDP-Assignment-8/test/UserModelTest.java
+++ Assignments/PDP-Assignment-8/test/UserModelTest.java
@@ -1,11 +1,52 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import virtualgambling.model.SimpleUserModel;
+import virtualgambling.model.UserModel;
+import virtualgambling.model.bean.Portfolio;
+import virtualgambling.model.stockdatasource.SimpleStockExchange;
+import virtualgambling.model.stockexchange.SimpleStockDataSource;
 
 /**
  * Created by gajjar.s, on 9:52 PM, 11/12/18
  */
 public class UserModelTest {
+
   @Test
   public void testInitializationOfUserModel() {
+    UserModel userModel = getUserModel();
+    Assert.assertEquals(0, userModel.getAllPortfolios().size());
+    Assert.assertNull(userModel.getPortfolio("test"));
 
+    userModel.createPortfolio("test");
+    Portfolio portfolio = userModel.getPortfolio("test");
+
+    Assert.assertNotNull(portfolio);
+    Assert.assertEquals("test", portfolio.getName());
+    Assert.assertEquals(0, portfolio.getPurchases().size());
+    Assert.assertEquals(BigDecimal.ZERO, portfolio.getCostBasis());
+    Assert.assertEquals(BigDecimal.ZERO, portfolio.getPortfolioValue());
+
+    Assert.assertEquals(new BigDecimal("1000000"), userModel.getRemainingCapital());
+  }
+
+  @Test
+  public void getPortfolioAndGetAllPortfolioWorks() {
+    UserModel userModel = getUserModel();
+
+    for (int i = 1; i <= 4; i++) {
+      String portfolioName = String.format("p%d", i);
+      userModel.createPortfolio(portfolioName);
+      Portfolio portfolio = userModel.getPortfolio(portfolioName);
+      Assert.assertEquals(portfolioName, portfolio.getName());
+    }
+
+    Assert.assertEquals(4, userModel.getAllPortfolios().size());
+  }
+
+  private UserModel getUserModel() {
+    return new SimpleUserModel(new SimpleStockExchange(new SimpleStockDataSource()));
   }
 }
