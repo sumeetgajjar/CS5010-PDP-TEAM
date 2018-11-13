c7272e871cee02a76f8bbb88386336c97b1cf5c6
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-12 20:53:49 -0500

    added controller and view interface

---
+++ Assignments/PDP-Assignment-8/src/virtualgambling/controller/Controller.java
@@ -0,0 +1,7 @@
+package virtualgambling.controller;
+
+/**
+ * Created by gajjar.s, on 8:42 PM, 11/12/18
+ */
+public interface Controller {
+}
---
+++ Assignments/PDP-Assignment-8/src/virtualgambling/view/View.java
@@ -0,0 +1,7 @@
+package virtualgambling.view;
+
+/**
+ * Created by gajjar.s, on 8:42 PM, 11/12/18
+ */
+public interface View {
+}
