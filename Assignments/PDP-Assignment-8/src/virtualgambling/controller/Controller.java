91f60b880ee837c04e5775f4697f859cb7ef1078
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-12 20:58:10 -0500

    1. added methods in controller and view interface

--- Assignments/PDP-Assignment-8/src/virtualgambling/controller/Controller.java
+++ Assignments/PDP-Assignment-8/src/virtualgambling/controller/Controller.java
@@ -4,4 +4,6 @@ package virtualgambling.controller;
  * Created by gajjar.s, on 8:42 PM, 11/12/18
  */
 public interface Controller {
+
+  void go();
 }
--- Assignments/PDP-Assignment-8/src/virtualgambling/view/View.java
+++ Assignments/PDP-Assignment-8/src/virtualgambling/view/View.java
@@ -4,4 +4,8 @@ package virtualgambling.view;
  * Created by gajjar.s, on 8:42 PM, 11/12/18
  */
 public interface View {
+
+  String getInput();
+
+  void display(String text);
 }
