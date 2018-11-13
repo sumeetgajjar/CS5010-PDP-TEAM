4572fbaf16d3f0824f823853d733cfa1cdbaa64d
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-12 21:40:53 -0500

    made datasource public

--- Assignments/PDP-Assignment-8/src/virtualgambling/model/StockExchange.java
+++ Assignments/PDP-Assignment-8/src/virtualgambling/model/StockExchange.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import virtualgambling.model.bean.Share;
 import virtualgambling.model.exceptions.StockPriceNotFoundException;
 
-interface StockExchange {
+public interface StockExchange {
   Share getPrice(String tickerName, Date date) throws StockPriceNotFoundException;
 
   void addShareData(Share share, Date date);
