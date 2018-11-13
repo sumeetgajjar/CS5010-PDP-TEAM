eaf4f1c7914caf9b4f379a4893f4d4d5a9930ff3
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-12 20:05:19 -0500

    1. naming refactoring

--- Assignments/PDP-Assignment-8/src/virtualgambling/model/bean/PurchaseInfo.java
+++ Assignments/PDP-Assignment-8/src/virtualgambling/model/bean/PurchaseInfo.java
@@ -1,12 +1,12 @@
 package virtualgambling.model.bean;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * Created by gajjar.s, on 8:11 PM, 11/11/18
  */
 public class PurchaseInfo {
   private Share share;
-  private LocalDateTime purchaseDateTime;
+  private Date date;
   private long quantity;
 }
