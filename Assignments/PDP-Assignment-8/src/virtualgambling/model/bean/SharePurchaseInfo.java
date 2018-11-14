bffb48769bad72ab5668f38a13e00d755257b9b8
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-14 15:27:26 -0500

    1. added documentation for SharePurchaseInfo.java

--- Assignments/PDP-Assignment-8/src/virtualgambling/model/bean/SharePurchaseInfo.java
+++ Assignments/PDP-Assignment-8/src/virtualgambling/model/bean/SharePurchaseInfo.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import util.Utils;
 
 /**
- * Created by gajjar.s, on 8:11 PM, 11/11/18
+ * This class represents a information which is captured when a Share is purchased.
  */
 public class SharePurchaseInfo {
   private final String tickerName;
@@ -14,6 +14,14 @@ public class SharePurchaseInfo {
   private final Date date;
   private final long quantity;
 
+  /**
+   * Constructs a Object of {@link SharePurchaseInfo} with the given params.
+   *
+   * @param tickerName the name of the share purchased
+   * @param unitPrice  the unit price of the share purchased
+   * @param date       the date at which the share was purchased
+   * @param quantity   the quantity of the share purchased
+   */
   public SharePurchaseInfo(String tickerName, BigDecimal unitPrice, Date date, long quantity) {
     this.tickerName = tickerName;
     this.unitPrice = unitPrice;
@@ -21,22 +29,47 @@ public class SharePurchaseInfo {
     this.quantity = quantity;
   }
 
+  /**
+   * Returns the tickerName of the Share.
+   *
+   * @return the tickerName of the Share
+   */
   public String getTickerName() {
     return tickerName;
   }
 
+  /**
+   * Returns the unit price of the Share.
+   *
+   * @return the unit price of the Share
+   */
   public BigDecimal getUnitPrice() {
     return unitPrice;
   }
 
+  /**
+   * Returns the date at which the share was purchased.
+   *
+   * @return the date at which the share was purchased
+   */
   public Date getDate() {
     return date;
   }
 
+  /**
+   * Returns the quantity of the shares purchased.
+   *
+   * @return the quantity of the shares purchased
+   */
   public long getQuantity() {
     return quantity;
   }
 
+  /**
+   * Returns the total cost of this purchase.
+   *
+   * @return the total cost of this purchase
+   */
   public BigDecimal getCostOfPurchase() {
     return this.unitPrice.multiply(BigDecimal.valueOf(quantity));
   }
