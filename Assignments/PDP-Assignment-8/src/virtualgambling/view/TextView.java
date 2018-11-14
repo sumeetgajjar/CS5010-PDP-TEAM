8d159ddd9280b9d754da19ca8fa0a3d38772983d
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-13 23:40:34 -0500

    1. added utils

--- Assignments/PDP-Assignment-8/src/virtualgambling/view/TextView.java
+++ Assignments/PDP-Assignment-8/src/virtualgambling/view/TextView.java
@@ -1,16 +1,24 @@
 package virtualgambling.view;
 
+import virtualgambling.util.Utils;
+
 /**
  * Created by gajjar.s, on 9:42 PM, 11/12/18
  */
 public class TextView implements View {
 
-  public TextView(Readable readable, Appendable appendable) {
+  private final Readable readable;
+  private final Appendable appendable;
 
+  public TextView(Readable readable, Appendable appendable) throws IllegalArgumentException {
+    this.readable = Utils.requireNonNull(readable);
+    this.appendable = Utils.requireNonNull(appendable);
   }
 
   @Override
   public String getInput() {
+
+
     return null;
   }
 
