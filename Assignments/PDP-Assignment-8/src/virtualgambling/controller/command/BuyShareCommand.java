241ac5f588f8e5584718de09ee23e1c4a06d9612
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-14 14:36:28 -0500

    1. added documentation for BuyShareCommand.java

--- Assignments/PDP-Assignment-8/src/virtualgambling/controller/command/BuyShareCommand.java
+++ Assignments/PDP-Assignment-8/src/virtualgambling/controller/command/BuyShareCommand.java
@@ -5,6 +5,9 @@ import java.util.function.Consumer;
 
 import virtualgambling.model.UserModel;
 
+/**
+ * This class represents a Buy Share command. It implements the {@link Command} interface.
+ */
 public class BuyShareCommand implements Command {
   private final String tickerName;
   private final String portfolioName;
@@ -12,6 +15,15 @@ public class BuyShareCommand implements Command {
   private final long quantity;
   private final Consumer<String> consumer;
 
+  /**
+   * Constructs a Object of {@link BuyShareCommand} with the given params.
+   *
+   * @param tickerName    the tickerName of the stock to buy
+   * @param portfolioName the portfolioName in which the stock should be bought
+   * @param date          the date at which stock should be bought
+   * @param quantity      the number of shares to buy
+   * @param consumer      the consumer of consume the result of command
+   */
   public BuyShareCommand(String tickerName, String portfolioName, Date date, long quantity,
                          Consumer<String> consumer) {
     this.tickerName = tickerName;
@@ -21,6 +33,11 @@ public class BuyShareCommand implements Command {
     this.consumer = consumer;
   }
 
+  /**
+   * Executes the command and consumes the result of the command using the consumer.
+   *
+   * @param userModel the userModel
+   */
   @Override
   public void execute(UserModel userModel) {
     this.consumer.accept(userModel.buyShares(tickerName, portfolioName, date,
