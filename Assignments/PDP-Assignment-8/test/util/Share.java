24da0f19e854ad6be0a403847583a6f0ba63e40e
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-14 15:33:26 -0500

    1. added documentation for Share.java

--- Assignments/PDP-Assignment-8/test/util/Share.java
+++ Assignments/PDP-Assignment-8/test/util/Share.java
@@ -4,21 +4,37 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 /**
- * Created by gajjar.s, on 10:11 PM, 11/11/18
+ * This class represents a Share. A share has a tickerName and a UnitPrice associated with it.
  */
 public class Share {
   private final String tickerName;
   private final BigDecimal unitPrice;
 
+  /**
+   * Constructs a Object of {@link Share} with the given params.
+   *
+   * @param tickerName the tickerName of the share
+   * @param unitPrice  the unitPrice of the share
+   */
   public Share(String tickerName, BigDecimal unitPrice) {
     this.tickerName = tickerName;
     this.unitPrice = unitPrice;
   }
 
+  /**
+   * Returns the ticker name of this share.
+   *
+   * @return the ticker name of this share
+   */
   public String getTickerName() {
     return tickerName;
   }
 
+  /**
+   * Returns the unit price of this share.
+   *
+   * @return the unit price of this share
+   */
   public BigDecimal getUnitPrice() {
     return unitPrice;
   }
