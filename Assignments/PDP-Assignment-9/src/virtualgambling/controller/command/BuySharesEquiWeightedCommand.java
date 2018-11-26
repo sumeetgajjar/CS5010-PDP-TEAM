edb0227c9226014035d844f149596b4f9dba1168
Chintan Shah <chnsh24@gmail.com>
2018-11-26 18:57:06 -0500

    Refactored BuyShareWeightedCommand

--- Assignments/PDP-Assignment-9/src/virtualgambling/controller/command/BuySharesEquiWeightedCommand.java
+++ Assignments/PDP-Assignment-9/src/virtualgambling/controller/command/BuySharesEquiWeightedCommand.java
@@ -1,8 +1,10 @@
 package virtualgambling.controller.command;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
+import util.Utils;
 
 /**
  * This class represents a Buy Share weighted command with the modification that each ticker has
@@ -13,13 +15,15 @@ public class BuySharesEquiWeightedCommand extends BuySharesWeightedCommand {
    * Constructs a BuySharesEquiWeightedCommand in terms of the tickerNames such that each ticker has
    * an equal weight.
    *
-   * @param tickerNames list of ticker names
+   * @param tickerNames set of ticker names
    */
-  public BuySharesEquiWeightedCommand(List<String> tickerNames) {
-    super(tickerNames,
-            IntStream.range(0, tickerNames.size())
-                    .mapToDouble(i -> 100.0 / tickerNames.size())
-                    .boxed()
-                    .collect(Collectors.toList()));
+  public BuySharesEquiWeightedCommand(Set<String> tickerNames) {
+    super(getTickersWithWeights(Utils.requireNonNull(tickerNames)));
+  }
+
+  private static Map<String, Double> getTickersWithWeights(Set<String> tickerNames) {
+    double weight = 100.0 / tickerNames.size();
+    return tickerNames.stream().collect(Collectors.toMap(stock -> stock,
+            stock -> weight));
   }
 }
