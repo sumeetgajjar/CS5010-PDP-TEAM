60bdf62f05fa12c813fe165c8e0e096e949ba0de
Chintan Shah <chnsh24@gmail.com>
2018-11-26 16:41:57 -0500

    Added strategy interface

--- Assignments/PDP-Assignment-9/src/virtualgambling/model/stockdatasource/strategy/Strategy.java
+++ Assignments/PDP-Assignment-9/src/virtualgambling/model/stockdatasource/strategy/Strategy.java
@@ -1,7 +1,27 @@
 package virtualgambling.model.stockdatasource.strategy;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import virtualgambling.model.bean.SharePurchaseOrder;
+
 /**
- * Created by gajjar.s, on 1:11 PM, 11/25/18
+ * {@link Strategy} represents a strategy of investing in stocks given the amount to invest and
+ * returns a list of {@link SharePurchaseOrder}.
  */
 public interface Strategy {
+  /**
+   * Executes a strategy such that the amount to invest is passed as input and the strategy
+   * generates a list of purchase orders.
+   *
+   * <p>It throws an {@link IllegalArgumentException} if the following cases occur:
+   * <ul>
+   * <li>amountToInvest is null</li>
+   * <li>amountToInvest is negative</li>
+   * </ul>
+   *
+   * @param amountToInvest amount to invest in dollars
+   * @return list of {@link SharePurchaseOrder}.
+   */
+  List<SharePurchaseOrder> execute(BigDecimal amountToInvest) throws IllegalArgumentException;
 }
