4cde51680a3e539f7343fe2fa5df5ebc63b95b83
Chintan Shah <chnsh24@gmail.com>
2018-11-26 20:05:22 -0500

    Total weight should sum to 100 and not 1

--- Assignments/PDP-Assignment-9/src/virtualgambling/model/strategy/WeightedInvestmentStrategy.java
+++ Assignments/PDP-Assignment-9/src/virtualgambling/model/strategy/WeightedInvestmentStrategy.java
@@ -17,7 +17,7 @@ public class WeightedInvestmentStrategy implements Strategy {
 
   private void checkInvariantForStockWeights(Map<String, Double> stockWeights) {
     double weightSum = stockWeights.values().stream().mapToDouble(Double::doubleValue).sum();
-    if (!Utils.areTwoDoublesEqual(weightSum, 1.0, 0.001)) {
+    if (!Utils.areTwoDoublesEqual(weightSum, 100.0, 0.001)) {
       throw new IllegalArgumentException("Weights do not sum up to 1");
     }
   }
