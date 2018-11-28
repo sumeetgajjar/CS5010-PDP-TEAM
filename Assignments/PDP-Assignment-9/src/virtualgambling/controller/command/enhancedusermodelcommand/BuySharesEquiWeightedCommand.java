4a30a83ab16b6468b3053f48aea0323301eb00a6
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-28 11:42:40 -0500

    1. added implementation for BuySharesEquiWeightedCommand.java and BuySharesWeightedCommand.java

--- Assignments/PDP-Assignment-9/src/virtualgambling/controller/command/enhancedusermodelcommand/BuySharesEquiWeightedCommand.java
+++ Assignments/PDP-Assignment-9/src/virtualgambling/controller/command/enhancedusermodelcommand/BuySharesEquiWeightedCommand.java
@@ -1,5 +1,6 @@
 package virtualgambling.controller.command.enhancedusermodelcommand;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
@@ -7,6 +8,7 @@ import java.util.stream.Collectors;
 
 import util.Utils;
 import virtualgambling.controller.command.Command;
+import virtualgambling.model.EnhancedUserModel;
 
 /**
  * This class represents a Buy Share weighted command with the modification that each ticker has
@@ -17,11 +19,22 @@ public class BuySharesEquiWeightedCommand extends BuySharesWeightedCommand {
    * Constructs a BuySharesEquiWeightedCommand in terms of the tickerNames such that each ticker has
    * an equal weight.
    *
-   * @param dateOfPurchase the date of purchase for stocks
-   * @param tickerNames    set of ticker names
+   * @param enhancedUserModel the enhanced user model
+   * @param portfolioName     the portfolio name
+   * @param amountToInvest    the amount to invest
+   * @param dateOfPurchase    the date of purchase for stocks
+   * @param tickerNames       set of ticker names
+   * @param commission        the commission for each transaction
    */
-  public BuySharesEquiWeightedCommand(Date dateOfPurchase, Set<String> tickerNames) {
-    super(dateOfPurchase, getTickersWithWeights(Utils.requireNonNull(tickerNames)));
+  public BuySharesEquiWeightedCommand(EnhancedUserModel enhancedUserModel, String portfolioName,
+                                      BigDecimal amountToInvest, Date dateOfPurchase,
+                                      Set<String> tickerNames, double commission) {
+    super(enhancedUserModel,
+            portfolioName,
+            amountToInvest,
+            dateOfPurchase,
+            getTickersWithWeights(Utils.requireNonNull(tickerNames)),
+            commission);
   }
 
   private static Map<String, Double> getTickersWithWeights(Set<String> tickerNames) {
