5b5c56fb3ba6b63b0117dd1ca29d5eccf04239bb
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-28 11:29:01 -0500

    1. added AbstractUserModelCommand.java which implements Command Interface

---
+++ Assignments/PDP-Assignment-9/src/virtualgambling/controller/command/AbstractUserModelCommand.java
@@ -0,0 +1,21 @@
+package virtualgambling.controller.command;
+
+import virtualgambling.model.UserModel;
+
+/**
+ * This class represents a Abstract Command which has a {@link UserModel}. It implements {@link
+ * Command} interface.
+ */
+public abstract class AbstractUserModelCommand implements Command {
+
+  protected final UserModel userModel;
+
+  /**
+   * Constructs a {@link AbstractUserModelCommand} object with given params.
+   *
+   * @param userModel the user model
+   */
+  protected AbstractUserModelCommand(UserModel userModel) {
+    this.userModel = userModel;
+  }
+}
