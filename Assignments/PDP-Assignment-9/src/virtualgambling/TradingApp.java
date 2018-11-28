3728b175df87dbf372683035e3778137933cd955
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-28 14:12:12 -0500

    1. added functionality to run both simple and enhanced trading app side by side

--- Assignments/PDP-Assignment-9/src/virtualgambling/TradingApp.java
+++ Assignments/PDP-Assignment-9/src/virtualgambling/TradingApp.java
@@ -3,6 +3,7 @@ package virtualgambling;
 import java.io.InputStreamReader;
 
 import virtualgambling.controller.Controller;
+import virtualgambling.controller.OrchestratorController;
 import virtualgambling.controller.TradingController;
 import virtualgambling.model.SimpleUserModel;
 import virtualgambling.model.UserModel;
@@ -14,15 +15,26 @@ import virtualgambling.view.View;
 /**
  * The class represents a Runner for the VirtualGambling MVC app.
  */
-public class SimpleTradingApp {
+public class TradingApp {
 
   /**
-   * Initializes the Model, View and Controller and start the application.
+   * Initializes the Model, View and Controller and start the application. Starts the
+   * SimpleTradingApp if the args[0] is equal to "1", else starts EnhancedTradingApp by default.
    *
    * @param args the args
    */
   public static void main(String[] args) {
-    runSimpleTradingApp();
+    if (args.length == 1 && args[0].equalsIgnoreCase("1")) {
+      runSimpleTradingApp();
+    } else {
+      runEnhancedTradingApp();
+    }
+  }
+
+  private static void runEnhancedTradingApp() {
+    View view = new TextView(new InputStreamReader(System.in), System.out);
+    Controller controller = new OrchestratorController(view);
+    controller.run();
   }
 
   private static void runSimpleTradingApp() {
