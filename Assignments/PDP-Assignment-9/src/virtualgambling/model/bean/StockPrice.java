ed141d5a2974cda2cac1f8bffb71384d8c8965f4
Chintan Shah <chnsh24@gmail.com>
2018-11-29 15:29:14 -0500

    Documented stockPrice

--- Assignments/PDP-Assignment-9/src/virtualgambling/model/bean/StockPrice.java
+++ Assignments/PDP-Assignment-9/src/virtualgambling/model/bean/StockPrice.java
@@ -3,19 +3,41 @@ package virtualgambling.model.bean;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import util.Utils;
+
+/**
+ * Represents stock price and is expressed in terms of it's date and the unit price.
+ */
 public class StockPrice {
   private final Date date;
   private final BigDecimal stockPrice;
 
-  public StockPrice(BigDecimal stockPrice, Date date) {
-    this.date = date;
-    this.stockPrice = stockPrice;
+  /**
+   * Constructs a stockPrice in terms of it's date and stockPrice.
+   *
+   * @param stockPrice stock price in terms of bigDecimal
+   * @param date       the date
+   * @throws IllegalArgumentException if any inputs are null
+   */
+  public StockPrice(BigDecimal stockPrice, Date date) throws IllegalArgumentException {
+    this.date = Utils.requireNonNull(date);
+    this.stockPrice = Utils.requireNonNull(stockPrice);
   }
 
+  /**
+   * Fetches the date of the stock price.
+   *
+   * @return date of the stock price
+   */
   public Date getDate() {
     return date;
   }
 
+  /**
+   * Fetches the unit price of the stock.
+   *
+   * @return unit price of the stock
+   */
   public BigDecimal getUnitPrice() {
     return stockPrice;
   }
