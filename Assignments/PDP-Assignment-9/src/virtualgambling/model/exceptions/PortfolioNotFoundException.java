11f88f40a0ae5e4027ad0c6b102c9567bccda75e
gajjar.s <gajjar.s@husky.neu.edu>
2018-11-29 00:59:43 -0500

    1. added documentation for AlphaVantageAPILimitExceeded.java

--- Assignments/PDP-Assignment-9/src/virtualgambling/model/exceptions/PortfolioNotFoundException.java
+++ Assignments/PDP-Assignment-9/src/virtualgambling/model/exceptions/PortfolioNotFoundException.java
@@ -1,6 +1,16 @@
 package virtualgambling.model.exceptions;
 
+/**
+ * This class represents a runtime exception which will be thrown if a portfolio queried is not
+ * present in the UserModel.
+ */
 public class PortfolioNotFoundException extends RuntimeException {
+
+  /**
+   * Constructs an Object of PortfolioNotFoundException with the given message.
+   *
+   * @param message the given message
+   */
   public PortfolioNotFoundException(String message) {
     super(message);
   }
