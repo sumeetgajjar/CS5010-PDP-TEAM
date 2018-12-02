e0a9ad4dda569c93d35905296d50a6d9d9fcb613
gajjar.s <gajjar.s@husky.neu.edu>
2018-12-02 14:34:55 -0500

    1. added documentation for the newly added class

--- Assignments/PDP-Assignment-10/src/main/java/virtualgambling/model/UserModelV3Impl.java
+++ Assignments/PDP-Assignment-10/src/main/java/virtualgambling/model/UserModelV3Impl.java
@@ -22,15 +22,37 @@ public class UserModelV3Impl extends EnhancedUserModelImpl implements UserModelV
     super(stockDAO);
   }
 
+  /**
+   * Saves the given portfolio in a file at the give path.
+   *
+   * @param portfolioName the portfolio to save
+   * @param path          the path of the file to save portfolio
+   * @throws IOException              if unable to save the portfolio to file
+   * @throws IllegalArgumentException if the any of the given params are null or if the portfolio
+   *                                  does not exists in this model
+   */
   @Override
-  public void savePortfolioToFile(String portfolioName, Path path) throws IOException {
+  public void savePortfolioToFile(String portfolioName, Path path) throws IOException,
+          IllegalArgumentException {
     Portfolio portfolio = getPortfolio(portfolioName);
     String serializedString = portfolio.serialize();
     Utils.saveToFile(path, serializedString);
   }
 
+  //todo use a specific exception
+
+  /**
+   * Load the portfolio from the file at the given path into this Model. It throws {@link
+   * IllegalStateException} if a portfolio already exists with the name as same the name of the
+   * portfolio which is being loaded from the file.
+   *
+   * @param path the path of the file
+   * @throws IOException              if unable to read from the file
+   * @throws IllegalArgumentException if the given path is null
+   */
   @Override
-  public void loadPortfolioFromFileInModel(Path path) throws IOException, IllegalStateException {
+  public void loadPortfolioFromFileInModel(Path path) throws IOException, IllegalArgumentException,
+          IllegalStateException {
     String serializedString = Utils.readStringFromFile(path);
     Portfolio portfolio = Portfolio.deserialize(serializedString);
     if (this.portfolios.containsKey(portfolio.getName())) {
