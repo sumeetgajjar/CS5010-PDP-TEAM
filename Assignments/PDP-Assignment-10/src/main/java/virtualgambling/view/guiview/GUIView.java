c321b0204907fb12d7b33a14e3c9adae5209f93d
gajjar.s <gajjar.s@husky.neu.edu>
2018-12-06 07:09:30 -0500

    1, added documentation for the GUIView.java

--- Assignments/PDP-Assignment-10/src/main/java/virtualgambling/view/guiview/GUIView.java
+++ Assignments/PDP-Assignment-10/src/main/java/virtualgambling/view/guiview/GUIView.java
@@ -4,11 +4,23 @@ import virtualgambling.controller.Features;
 import virtualgambling.view.View;
 
 /**
- * Created by gajjar.s, on 3:42 AM, 12/5/18
+ * This class represents an GUI View of the MVC application. It extends the traditional {@link View}
+ * interface and adds functionality of reporting errors and a way to execute callbacks of
+ * controller.
  */
 public interface GUIView extends View {
 
+  /**
+   * Sets the given Features, available for this View.
+   *
+   * @param features the feature available to this view
+   */
   void addFeatures(Features features);
 
+  /**
+   * Displays the given message as error to the user.
+   *
+   * @param message the given error message
+   */
   void displayError(String message);
 }
