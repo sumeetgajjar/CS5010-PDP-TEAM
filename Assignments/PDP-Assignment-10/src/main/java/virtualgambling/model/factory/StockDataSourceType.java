05076e281b7934b015fb2fa84ecf447057e9550b
Chintan Shah <chnsh24@gmail.com>
2018-12-06 14:03:23 -0500

    Documented factory package

--- Assignments/PDP-Assignment-10/src/main/java/virtualgambling/model/factory/StockDataSourceType.java
+++ Assignments/PDP-Assignment-10/src/main/java/virtualgambling/model/factory/StockDataSourceType.java
@@ -1,5 +1,12 @@
 package virtualgambling.model.factory;
 
+import util.Utils;
+import virtualgambling.model.stockdatasource.StockDataSource;
+
+/**
+ * {@link StockDataSourceType} is an enum that represents the type of {@link StockDataSource} to
+ * use.
+ */
 public enum StockDataSourceType {
   SIMPLE("In Memory"),
   ALPHA_VANTAGE("Alpha Vantage"),
@@ -7,10 +14,20 @@ public enum StockDataSourceType {
 
   private final String name;
 
+  /**
+   * Constructs a {@link StockDataSourceType} given the name of the stock data source type.
+   *
+   * @param name the name of the data source type as a string
+   */
   StockDataSourceType(String name) {
-    this.name = name;
+    this.name = Utils.requireNonNull(name);
   }
 
+  /**
+   * Gets the name of the data source type in String representation form.
+   *
+   * @return the string representation of the name of the data source type
+   */
   public String getName() {
     return name;
   }
