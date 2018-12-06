f5e80f1a0da4e96c53e1bca64473e38f3c8c57bb
gajjar.s <gajjar.s@husky.neu.edu>
2018-12-06 13:30:01 -0500

    removed individual test files and move those tests to MiscellaneousTests.java

--- Assignments/PDP-Assignment-10/src/test/java/MiscellaneousTests.java
+++ Assignments/PDP-Assignment-10/src/test/java/MiscellaneousTests.java
@@ -1,8 +1,12 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -10,8 +14,10 @@ import java.util.Random;
 
 import util.Constants;
 import util.TestUtils;
+import util.Utils;
 import virtualgambling.controller.Controller;
 import virtualgambling.controller.EnhancedTradingController;
+import virtualgambling.controller.OrchestratorController;
 import virtualgambling.controller.TradingController;
 import virtualgambling.model.EnhancedUserModel;
 import virtualgambling.model.UserModel;
@@ -24,16 +30,52 @@ import virtualgambling.model.exceptions.StockDataNotFoundException;
 import virtualgambling.model.exceptions.StrategyExecutionException;
 import virtualgambling.model.factory.StockDAOType;
 import virtualgambling.model.factory.StockDataSourceType;
+import virtualgambling.model.stockdatasource.AlphaVantageAPIStockDataSource;
+import virtualgambling.model.stockdatasource.StockDataSource;
 import virtualgambling.model.strategy.Strategy;
 import virtualgambling.view.TextView;
+import virtualgambling.view.View;
 
 /**
  * The class represents a Junit class to test wiring of Model controller and view in isolation.
  */
-public class ModelControllerViewWiringTest {
+public class MiscellaneousTests {
 
   private static final Random RANDOM = new Random(System.currentTimeMillis());
 
+  private StockDataSource dataSource;
+
+  @Before
+  public void setUp() {
+    dataSource = AlphaVantageAPIStockDataSource.getInstance();
+  }
+
+  // start of view tests
+  @Test
+  public void getInputFromViewWorks() throws IOException {
+    Readable readable = new StringReader("line1\nline2\n");
+    Appendable appendable = new StringBuffer();
+
+    View view = new TextView(readable, appendable);
+    Assert.assertEquals("line1", view.getInput());
+    Assert.assertEquals("line2", view.getInput());
+  }
+
+  @Test
+  public void displayTextOnViewWorks() throws IOException {
+    Readable readable = new StringReader("");
+    Appendable appendable = new StringBuffer();
+
+    View view = new TextView(readable, appendable);
+    view.display("line1");
+    Assert.assertEquals("line1", appendable.toString());
+
+    view.display("line2");
+    Assert.assertEquals("line1line2", appendable.toString());
+  }
+  // end of view tests
+
+  // start of ModelControllerView Wiring tests
   @Test
   public void testingUserModelControllerViewWiring() {
     Readable readable = new StringReader("1 p1 3 p1 2018-11-11 4 p1 2018-11-11 5 p1 2 6 7 AAPL p1" +
@@ -267,6 +309,115 @@ public class ModelControllerViewWiringTest {
 
     Assert.assertEquals(expectedOutput.toString(), appendable.toString());
   }
+  // end of ModelControllerView Wiring tests
+
+
+  // start of OrchestratorController tests
+  @Test
+  public void quitWorks() {
+    for (String quit : Arrays.asList("q", "quit")) {
+      Readable readable = new StringReader(quit);
+      Appendable appendable = new StringBuffer();
+      View view = new TextView(readable, appendable);
+      Controller controller = new OrchestratorController(view);
+      controller.run();
+
+      String expectedOutput =
+              TestUtils.getWelcomeMessage() + System.lineSeparator()
+                      + TestUtils.getMenuMessageOfOrchestrator() +
+                      System.lineSeparator();
+      Assert.assertEquals(expectedOutput, appendable.toString());
+    }
+  }
+
+  @Test
+  public void invalidInputAsksForRetry() {
+    Readable readable = new StringReader("asd q");
+    Appendable appendable = new StringBuffer();
+    View view = new TextView(readable, appendable);
+    Controller controller = new OrchestratorController(view);
+    controller.run();
+
+    String expectedOutput =
+            TestUtils.getWelcomeMessage() + System.lineSeparator()
+                    + TestUtils.getMenuMessageOfOrchestrator()
+                    + System.lineSeparator() + Constants.INVALID_CHOICE_MESSAGE
+                    + System.lineSeparator()
+                    + TestUtils.getMenuMessageOfOrchestrator() +
+                    System.lineSeparator();
+    Assert.assertEquals(expectedOutput, appendable.toString());
+  }
+
+  @Test
+  public void dataSourceSelectionWorks() {
+    for (int i : Arrays.asList(1, 2)) {
+      Readable readable = new StringReader(i + " quit");
+      Appendable appendable = new StringBuffer();
+      View view = new TextView(readable, appendable);
+      Controller controller = new OrchestratorController(view);
+      controller.run();
+
+      String expectedOutput =
+              TestUtils.getWelcomeMessage() + System.lineSeparator()
+                      + TestUtils.getMenuMessageOfOrchestrator()
+                      + System.lineSeparator()
+                      + TestUtils.getMenuMessageOfPersistableTradingController()
+                      + System.lineSeparator();
+      Assert.assertEquals(expectedOutput, appendable.toString());
+    }
+  }
+  // end of OrchestratorController tests
+
+
+  // start of AlphaVantageAPIStockDataSource tests
+  @Test
+  public void getPriceOnHolidayOfValidStockGetsNextAvailableDay() {
+    Calendar calendar = Utils.getCalendarInstance();
+    calendar.set(2018, Calendar.NOVEMBER, 22); // thanksgiving goes to friday
+    BigDecimal closePriceOn23Nov = dataSource.getPrice("AAPL", calendar.getTime()).getUnitPrice();
+    Assert.assertEquals(new BigDecimal("172.29"), closePriceOn23Nov.stripTrailingZeros());
+
+    calendar.set(2018, Calendar.NOVEMBER, 24); // saturday goes to monday
+    BigDecimal closePriceOn26Nov = dataSource.getPrice("AAPL", calendar.getTime()).getUnitPrice();
+    Assert.assertEquals(new BigDecimal("174.62"), closePriceOn26Nov.stripTrailingZeros());
+  }
+
+  @Test
+  public void aFutureDayFails() {
+    Calendar calendar = Utils.getCalendarInstance();
+    calendar.set(2118, Calendar.NOVEMBER, 22);
+    try {
+      dataSource.getPrice("AAPL", calendar.getTime());
+      Assert.fail("should have failed");
+    } catch (StockDataNotFoundException e) {
+      Assert.assertEquals("Stock Data Not found for: AAPL for 2118-11-22", e.getMessage());
+    }
+  }
+
+  @Test
+  public void priceReturnedIsClosingPrice() {
+    Date validDateForTrading = TestUtils.getValidDateForTrading();
+    BigDecimal aaplPrice = dataSource.getPrice("AAPL", validDateForTrading).getUnitPrice();
+    BigDecimal expectedAAPLClosingPrice = new BigDecimal("222.2200");
+    Assert.assertEquals(expectedAAPLClosingPrice, aaplPrice);
+  }
+
+  @Test
+  public void reallyOldDateFails() {
+    Calendar calendar = Utils.getCalendarInstance();
+    calendar.set(Calendar.YEAR, 1900);
+    calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+    calendar.set(Calendar.DAY_OF_MONTH, 2);
+    Date validDateForTrading = calendar.getTime();
+    try {
+      dataSource.getPrice("AAPL", validDateForTrading).getUnitPrice();
+      Assert.fail("should have failed");
+    } catch (StockDataNotFoundException e) {
+      Assert.assertEquals("Stock Data Not found for: AAPL for 1900-12-02", e.getMessage());
+    }
+  }
+  // end of AlphaVantageAPIStockDataSource tests
+
 
   private static class MockModel implements UserModel {
 
