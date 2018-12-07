db43c1d6761a6375675227256efd05ffaed2e6fd
Chintan Shah <chnsh24@gmail.com>
2018-12-06 19:04:42 -0500

    Made a decorator to hide inner implementation

--- Assignments/PDP-Assignment-10/src/main/java/virtualgambling/model/PersistableUserModelImpl.java
+++ Assignments/PDP-Assignment-10/src/main/java/virtualgambling/model/PersistableUserModelImpl.java
@@ -1,12 +1,22 @@
 package virtualgambling.model;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
 import util.Utils;
+import virtualgambling.model.bean.Portfolio;
+import virtualgambling.model.bean.SharePurchaseOrder;
+import virtualgambling.model.exceptions.InsufficientCapitalException;
+import virtualgambling.model.exceptions.PortfolioNotFoundException;
+import virtualgambling.model.exceptions.StockDataNotFoundException;
+import virtualgambling.model.exceptions.StrategyExecutionException;
 import virtualgambling.model.factory.StockDAOType;
 import virtualgambling.model.factory.StockDataSourceType;
 import virtualgambling.model.persistence.Loader;
 import virtualgambling.model.persistence.Persister;
+import virtualgambling.model.strategy.Strategy;
 
 /**
  * This class represents an enhancement over {@link EnhancedUserModelImpl}. It implements {@link
@@ -30,19 +40,13 @@ public class PersistableUserModelImpl extends EnhancedUserModelImpl
   }
 
   @Override
-  public void createPortfolio(String portfolioName) throws IllegalArgumentException {
-    Utils.requireNonNull(portfolioName);
-    this.validateAndCreatePortfolio(portfolioName);
-  }
-
-  @Override
   public void persistFromModel(Persister persister) throws IOException {
     persister.persist();
   }
 
   @Override
   public void loadIntoModel(Loader loader) throws IOException {
-    loader.load(this);
+    loader.load(new InnerDecorator(this));
   }
 
   @Override
@@ -63,5 +67,83 @@ public class PersistableUserModelImpl extends EnhancedUserModelImpl
   @Override
   public void setStockDataSourceType(StockDataSourceType stockDataSourceType) {
     this.stockDataSourceType = Utils.requireNonNull(stockDataSourceType);
+  }
+
+  private class InnerDecorator implements PersistableUserModel {
+    private final PersistableUserModelImpl persistable;
+
+    private InnerDecorator(PersistableUserModelImpl persistableUserModel) {
+      this.persistable = persistableUserModel;
+    }
+
+    @Override
+    public void createPortfolio(String portfolioName) throws IllegalArgumentException {
+      Utils.requireNonNull(portfolioName);
+      persistable.validateAndCreatePortfolio(portfolioName);
+    }
+
+    @Override
+    public Portfolio getPortfolio(String portfolioName) throws PortfolioNotFoundException {
+      return persistable.getPortfolio(portfolioName);
+    }
+
+    @Override
+    public List<Portfolio> getAllPortfolios() {
+      return persistable.getAllPortfolios();
+    }
+
+    @Override
+    public SharePurchaseOrder buyShares(String tickerName, String portfolioName, Date date,
+                                        long quantity) throws IllegalArgumentException,
+            StockDataNotFoundException, InsufficientCapitalException {
+      return persistable.buyShares(tickerName, portfolioName, date, quantity);
+    }
+
+    @Override
+    public BigDecimal getRemainingCapital() {
+      return persistable.getRemainingCapital();
+    }
+
+    @Override
+    public void persistFromModel(Persister persister) throws IOException {
+      persistable.persistFromModel(persister);
+    }
+
+    @Override
+    public void loadIntoModel(Loader loader) throws IOException {
+      persistable.loadIntoModel(loader);
+    }
+
+    @Override
+    public StockDAOType getStockDAOType() {
+      return persistable.getStockDAOType();
+    }
+
+    @Override
+    public StockDataSourceType getStockDataSourceType() {
+      return persistable.getStockDataSourceType();
+    }
+
+    @Override
+    public void setStockDAOType(StockDAOType stockDAOType) {
+      persistable.setStockDAOType(stockDAOType);
+    }
+
+    @Override
+    public void setStockDataSourceType(StockDataSourceType stockDataSourceType) {
+      persistable.setStockDataSourceType(stockDataSourceType);
+    }
+
+    @Override
+    public SharePurchaseOrder buyShares(String tickerName, String portfolioName, Date date,
+                                        long quantity, double commissionPercentage) throws IllegalArgumentException, StockDataNotFoundException, InsufficientCapitalException {
+      return persistable.buyShares(tickerName, portfolioName, date, quantity, commissionPercentage);
+    }
+
+    @Override
+    public List<SharePurchaseOrder> buyShares(String portfolioName, BigDecimal amountToInvest,
+                                              Strategy strategy, double commissionPercentage) throws IllegalArgumentException, StockDataNotFoundException, InsufficientCapitalException, StrategyExecutionException {
+      return persistable.buyShares(portfolioName, amountToInvest, strategy, commissionPercentage);
+    }
   }
 }
